482ade95d864927c447d1cac17bd1b35872f4e54
wesley-foussard <tktrekk@gmail.com>
2022-10-23 15:19:29 +0200

    correction des erreurs (ajout des fn dans le onmessage)

--- worker.js
+++ worker.js
@@ -2,19 +2,19 @@
 onmessage = (event) => {
   //on précise que les infos recus dans le message sont width et height
   const { width, height } = event.data;
-};
-function preparedisplay(event) {
-  for (let col = 0; col < event.width; col++) {
-    for (let row = 0; row < event.height; row++) {
+
+  for (let col = 0; col < width; col++) {
+    for (let row = 0; row < height; row++) {
       const color = getRandomColor();
       //on envoie au script un message avec row, col et color
       postMessage({ row, col, color });
     }
   }
-}
-function getRandomColor() {
-  const r = Math.floor(Math.random() * 128);
-  const g = 0;
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r},${g},${b})`;
-}
+
+  function getRandomColor() {
+    const r = Math.floor(Math.random() * 128);
+    const g = 0;
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+  }
+};
